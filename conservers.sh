eda9f1bf5d2c7058c6eb803fcf24e17c3a2ca37f
BRFSO <fernando.oliveira@sec4you.com.br>
2019-07-20 21:30:03 -0300

    Finally, working with command-line and gui mode, this true\!

--- conservers.sh
+++ conservers.sh
@@ -16,9 +16,9 @@ Usage:  $0 [OPTIONS]
 conServer
 
 Options:
- -u,  --update Update Server List
- -gm, --gui-mode      Use GUI Mode 
- -h,  --help           Displays usage info
+ -u,   --update        Update Server List
+ -gui, --gui-mode      Use GUI Mode 
+ -h,   --help          Displays usage info
 END
 )
 
@@ -77,52 +77,76 @@ if [ ! -f "$SERVERS_LIST" ] || [ "${UPDATE}" == 'true' ];
 
     fi
 fi
-#EMPRESAS=$(egrep  '^(.*;){3}[0-9]+;[^;]+$' "${SERVERS_LIST}" | grep -v ^# | awk -F";" '{print $1}' | sort -d | uniq)
-#clear
-#selectedEmploye=$(zenity --list --title="Ambientes" --width=400 --height=600 --column="Seleção" $EMPRESAS)
-##select selectedEmploye in $EMPRESAS; do
-#  if [ -z ${selectedEmploye} ];then
-#    echo "Opção Inválida, Digite a opção correta ou Crtl + C para sair: "
-#  else
-#    clear
-#    HOST=$(egrep  '^(.*;){3}[0-9]+;[^;]+$' "${SERVERS_LIST}" | grep -v ^# | grep -w $selectedEmploye | awk -F";" '{print $5}' | sort -d | uniq)
-#    echo "Selecione o servidor no qual você deseja se conectar: "
-#    selectedHost=$(zenity --list --title="Servidor" --width=400 --height=600 --column="Seleção" $HOST)
-#  #select selectedHost in $HOST; do
-#    if [ -z ${selectedHost} ];then
-#      echo "Opção Inválida, Digite a opção correta ou Crtl + C para sair: "
-#    else
-#      PORTA=$(egrep  '^(.*;){3}[0-9]+;[^;]+$' "${SERVERS_LIST}" | grep -v ^# | grep -w $selectedHost | awk -F";" '{print $4}')
-#      USUARIO=$(egrep  '^(.*;){3}[0-9]+;[^;]+$' "${SERVERS_LIST}" | grep -v ^# | grep -w $selectedHost | awk -F";" '{print $3}')
-#      SERVER=$(egrep  '^(.*;){3}[0-9]+;[^;]+$' "${SERVERS_LIST}" | grep -v ^# | grep -w $selectedHost | awk -F";" '{print $2}')
-#      OPTION=$(zenity --list --title="Conservice" --width=400 --height=600 \
-#          --column="Seleção" "Acesso Remoto" "Download/Upload")
-#      if [ "$OPTION" == "Acesso Remoto" ]
-#        then
-#          ssh -i $SSH_KEY -p"$PORTA" "$USUARIO"@"$SERVER"
-#      elif [ "$OPTION" == "Download/Upload" ]
-#        then
-#          MODE=$(zenity --list --title="Opção" --column="Seleção" "Upload" "Download")
-#          if [ "$MODE" == "Upload" ]
-#      then
-#            zenity --info --text="Selecione o arquivo para upload."
-#            FILE=$(zenity --file-selection --title="Selecione um arquivo")
-#            DEST=$(zenity --entry --text="Caminho do arquivo para upload no servidor:" --entry-text "Caminho")
-#            scp  $FILE $USUARIO@$SERVER:$DEST
-#            zenity --info --text="Concluído."
-#          elif [ "$MODE" == "Download" ]
-#      then
-#            zenity --info --text="Selecione diretório para receber o arquivo."
-#            FILE=$(zenity --file-selection --directory --title="Selecione um diretório")
-#            DEST=$(zenity --entry --text="Caminho do arquivo para download do servidor:" --entry-text "Caminho")
-#      #echo "scp $USUARIO@$SERVER:$DEST $FILE"
-#            scp $USUARIO@$SERVER:$DEST $FILE
-#            zenity --info --text="Concluído."
-#    fi
-#  fi
-#    #done
-#    fi
-#  #done
-#fi
-#
-#
+
+COMPANIES=$(egrep  '^(.*;){3}[0-9]+;[^;]+$' "${SERVERS_LIST}" | grep -v ^# | awk -F";" '{print $1}' | sort -d | uniq)
+clear
+
+if [ ${GUI_MODE} == "false" ];
+  then
+    select selectedCompany in $COMPANIES; 
+  do	
+    if [ -z ${selectedCompany} ];
+      then
+        echo "Opção Inválida, Digite a opção correta ou Crtl + C para sair: " 
+    else
+      clear
+      HOST=$(egrep  '^(.*;){3}[0-9]+;[^;]+$' "${SERVERS_LIST}" | grep -v ^# | grep $selectedCompany | awk -F";" '{print $5}' | uniq)
+      echo "Selecione o servidor no qual você deseja se conectar: "
+      select selectedHost in $HOST; 
+        do
+          if [ -z ${selectedHost} ];then
+            echo "Opção Inválida, Digite a opção correta ou Crtl + C para sair: " 
+          else
+            SERVER_PORT=$(egrep  '^(.*;){3}[0-9]+;[^;]+$' "${SERVERS_LIST}" | grep -v ^# | grep $selectedHost | awk -F";" '{print $4}')
+            SERVER_USER=$(egrep  '^(.*;){3}[0-9]+;[^;]+$' "${SERVERS_LIST}" | grep -v ^# | grep $selectedHost | awk -F";" '{print $3}')
+            SERVER=$(egrep  '^(.*;){3}[0-9]+;[^;]+$' "${SERVERS_LIST}" | grep -v ^# | grep $selectedHost | awk -F";" '{print $2}')
+            ssh -i $SSH_KEY -p"$SERVER_PORT" "$SERVER_USER"@"$SERVER"
+          fi
+        done
+    fi
+  done
+else
+   selectedCompany=$(zenity --list --title="Ambientes" --width=400 --height=600 --column="Seleção" $COMPANIES)
+    if [ -z ${selectedCompany} ];
+      then
+        echo "Opção Inválida, Digite a opção correta ou Crtl + C para sair: "
+    else
+      clear
+      HOST=$(egrep  '^(.*;){3}[0-9]+;[^;]+$' "${SERVERS_LIST}" | grep -v ^# | grep -w $selectedCompany | awk -F";" '{print $5}' | sort -d | uniq)
+      echo "Selecione o servidor no qual você deseja se conectar: "
+      selectedHost=$(zenity --list --title="Servidor" --width=400 --height=600 --column="Seleção" $HOST)
+      if [ -z ${selectedHost} ];
+        then
+          echo "Opção Inválida, Digite a opção correta ou Crtl + C para sair: "
+      else
+        SERVER_PORT=$(egrep  '^(.*;){3}[0-9]+;[^;]+$' "${SERVERS_LIST}" | grep -v ^# | grep -w $selectedHost | awk -F";" '{print $4}')
+        SERVER_USER=$(egrep  '^(.*;){3}[0-9]+;[^;]+$' "${SERVERS_LIST}" | grep -v ^# | grep -w $selectedHost | awk -F";" '{print $3}')
+        SERVER=$(egrep  '^(.*;){3}[0-9]+;[^;]+$' "${SERVERS_LIST}" | grep -v ^# | grep -w $selectedHost | awk -F";" '{print $2}')
+        OPTION=$(zenity --list --title="Conservice" --width=400 --height=600 \
+          --column="Seleção" "Acesso Remoto" "Download/Upload")
+        if [ "$OPTION" == "Acesso Remoto" ];
+          then
+            ssh -i $SSH_KEY -p"$SERVER_PORT" "$SERVER_USER"@"$SERVER"
+        elif [ "$OPTION" == "Download/Upload" ];
+          then
+            MODE=$(zenity --list --title="Opção" --column="Seleção" "Upload" "Download")
+            if [ "$MODE" == "Upload" ];
+              then
+                zenity --info --text="Selecione o arquivo para upload."
+                FILE=$(zenity --file-selection --title="Selecione um arquivo")
+                DEST=$(zenity --entry --text="Caminho do arquivo para upload no servidor:" --entry-text "Caminho")
+                scp  $FILE $SERVER_USER@$SERVER:$DEST
+                zenity --info --text="Concluído."
+            elif [ "$MODE" == "Download" ]
+              then
+                zenity --info --text="Selecione diretório para receber o arquivo."
+                FILE=$(zenity --file-selection --directory --title="Selecione um diretório")
+                DEST=$(zenity --entry --text="Caminho do arquivo para download do servidor:" --entry-text "Caminho")
+                scp $SERVER_USER@$SERVER:$DEST $FILE
+                zenity --info --text="Concluído."
+            fi
+        fi
+      fi
+    fi
+fi
+
